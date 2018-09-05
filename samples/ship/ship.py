53b516779e9cf215a234563f36fee583f62c9fd9
Gabriel Garza <garzagabriel@gmail.com>
2018-09-05 15:04:24 -0700

    epoch 160

--- samples/ship/ship.py
+++ samples/ship/ship.py
@@ -272,7 +272,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=155,
+                epochs=160,
                 layers='heads')
 
 def color_splash(image, mask):
